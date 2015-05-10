dbdd2de0cd9fe6ae3631573c8043fb838469732f
Lyysis <allix.amelie@gmail.com>
2015-05-10 19:40:39 +0200

    modification des fenetres room ajout/modification intervenant (finies) + modifications mineures d'autres fenêtres

--- Software/Apps/CatchingUpConfigTool/src/ui/SpeakerUpdate.java
+++ Software/Apps/CatchingUpConfigTool/src/ui/SpeakerUpdate.java
@@ -9,9 +9,26 @@ import javax.swing.JButton;
 import javax.swing.JDialog;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JPasswordField;
 import javax.swing.JTextField;
 import javax.swing.LayoutStyle.ComponentPlacement;
 import javax.swing.border.EmptyBorder;
+
+import java.awt.GridLayout;
+
+import javax.swing.SwingConstants;
+import javax.swing.BoxLayout;
+
+import java.awt.Component;
+import java.awt.GridBagLayout;
+import java.awt.GridBagConstraints;
+import java.awt.Insets;
+
+import net.miginfocom.swing.MigLayout;
+
+import java.awt.CardLayout;
+import javax.swing.JCheckBox;
+import javax.swing.JComboBox;
 
 public class SpeakerUpdate extends JDialog {
 	private String firstName;
@@ -25,15 +42,18 @@ public class SpeakerUpdate extends JDialog {
 	private final JPanel contentPanel = new JPanel();
 	private JTextField txtBoxFirstname;
 	private JTextField txtBoxLastname;
-	private JTextField txtBoxpassword;
+	private JPasswordField passwordFieldPassword;
 	private JTextField txtBoxEmail;
 	private JTextField txtBoxDateBegin;
 	private JTextField txtBoxDateEnd;
+	private JPasswordField passwordFieldConfirmPassword;
+	private JButton okButton;
+	private JButton cancelButton;
 
 	/**
 	 * Create the dialog.
 	 */
-	public SpeakerUpdate(String firstName, String lastName, String password, String email, String dateBegin, String dateEnd) {
+	public SpeakerUpdate(String firstName, String lastName, String email, String dateBegin, String dateEnd) {
 		setTitle("Modification intervenant");
 		this.firstName = firstName;
 		this.lastName = lastName;
@@ -42,102 +62,150 @@ public class SpeakerUpdate extends JDialog {
 		this.dateBegin = dateBegin;
 		this.dateEnd = dateEnd;
 		
-		setBounds(100, 100, 519, 413);
+		setBounds(100, 100, 735, 491);
 		getContentPane().setLayout(new BorderLayout());
 		contentPanel.setBorder(new EmptyBorder(5, 5, 5, 5));
 		getContentPane().add(contentPanel, BorderLayout.CENTER);
+		contentPanel.setLayout(new FlowLayout(FlowLayout.CENTER, 5, 5));
+		
+		JPanel TopPanel = new JPanel();
+		contentPanel.add(TopPanel);
+		TopPanel.setLayout(new GridLayout(0, 1, 0, 0));
+		
+		JPanel identityPanel = new JPanel();
+		TopPanel.add(identityPanel);
+		FlowLayout flowLayout = (FlowLayout) identityPanel.getLayout();
+		flowLayout.setVgap(10);
+		
+		JLabel lblNom = new JLabel("Nom :");
+		identityPanel.add(lblNom);
+		txtBoxLastname = new JTextField(lastName);
+		identityPanel.add(txtBoxLastname);
+		txtBoxLastname.setColumns(10);
+		
+		JLabel lblFirstName = new JLabel("Pr\u00E9nom :");
+		identityPanel.add(lblFirstName);
 		
 		txtBoxFirstname = new JTextField(firstName);
+		identityPanel.add(txtBoxFirstname);
 		txtBoxFirstname.setColumns(10);
-		txtBoxLastname = new JTextField(lastName);
-		txtBoxLastname.setColumns(10);
-		txtBoxpassword = new JTextField(password);
-		txtBoxpassword.setColumns(10);
+		
+		JPanel passwordPanel = new JPanel();
+		TopPanel.add(passwordPanel);
+		
+		JLabel lblPassword = new JLabel("Mot de passe :");
+		lblPassword.setHorizontalAlignment(SwingConstants.CENTER);
+		passwordPanel.add(lblPassword);
+		passwordFieldPassword = new JPasswordField(password);
+		passwordFieldPassword.setHorizontalAlignment(SwingConstants.CENTER);
+		passwordPanel.add(passwordFieldPassword);
+		passwordFieldPassword.setColumns(10);
+		
+		JLabel lblConfirmPassword = new JLabel("Confirmation mot de passe :");
+		lblConfirmPassword.setHorizontalAlignment(SwingConstants.CENTER);
+		passwordPanel.add(lblConfirmPassword);
+		
+		passwordFieldConfirmPassword = new JPasswordField();
+		passwordFieldConfirmPassword.setHorizontalAlignment(SwingConstants.CENTER);
+		passwordFieldConfirmPassword.setColumns(10);
+		passwordPanel.add(passwordFieldConfirmPassword);
+		
+		JPanel mailPanel = new JPanel();
+		TopPanel.add(mailPanel);
+		
+		JLabel lblEmail = new JLabel("Email :");
+		mailPanel.add(lblEmail);
 		txtBoxEmail = new JTextField(email);
-		txtBoxEmail.setColumns(10);
-		txtBoxDateBegin = new JTextField(dateBegin);
-		txtBoxDateBegin.setColumns(10);
-		txtBoxDateEnd = new JTextField(dateEnd);
-		txtBoxDateEnd.setColumns(10);
+		mailPanel.add(txtBoxEmail);
+		txtBoxEmail.setColumns(25);
 		
-		JLabel lblFirstName = new JLabel("Pr\u00E9nom");
+		JPanel rightPanel = new JPanel();
+		contentPanel.add(rightPanel);
+		rightPanel.setLayout(new BorderLayout(0, 0));
 		
-		JLabel lblNom = new JLabel("Nom");
+		JPanel rightPresentationPanel = new JPanel();
+		rightPanel.add(rightPresentationPanel, BorderLayout.NORTH);
+		rightPresentationPanel.setLayout(new FlowLayout(FlowLayout.CENTER, 5, 5));
 		
-		JLabel lblMotDePasse = new JLabel("Mot de passe");
+		JLabel lblNewLabel = new JLabel("Droits d'enregistrements de la personne");
+		rightPresentationPanel.add(lblNewLabel);
+		lblNewLabel.setHorizontalAlignment(SwingConstants.CENTER);
+		lblNewLabel.setVerticalAlignment(SwingConstants.TOP);
 		
-		JLabel lblEmail = new JLabel("email");
+		JPanel rightContentPanel = new JPanel();
+		rightPanel.add(rightContentPanel, BorderLayout.CENTER);
+		rightContentPanel.setLayout(new FlowLayout(FlowLayout.CENTER, 5, 5));
 		
-		JLabel lblDateBegin = new JLabel("Date de d\u00E9but");
+		JPanel calendarGridLayout = new JPanel();
+		rightPanel.add(calendarGridLayout, BorderLayout.WEST);
+		calendarGridLayout.setLayout(new GridLayout(1, 2, 0, 0));
 		
-		JLabel lblDateEnd = new JLabel("Date de fin");
-		GroupLayout gl_contentPanel = new GroupLayout(contentPanel);
-		gl_contentPanel.setHorizontalGroup(
-			gl_contentPanel.createParallelGroup(Alignment.TRAILING)
-				.addGroup(gl_contentPanel.createSequentialGroup()
-					.addContainerGap(140, Short.MAX_VALUE)
-					.addGroup(gl_contentPanel.createParallelGroup(Alignment.LEADING)
-						.addComponent(lblFirstName)
-						.addComponent(lblNom)
-						.addComponent(lblMotDePasse)
-						.addComponent(lblEmail)
-						.addComponent(lblDateBegin)
-						.addComponent(lblDateEnd))
-					.addGap(31)
-					.addGroup(gl_contentPanel.createParallelGroup(Alignment.LEADING)
-						.addComponent(txtBoxDateEnd, GroupLayout.PREFERRED_SIZE, 119, GroupLayout.PREFERRED_SIZE)
-						.addComponent(txtBoxDateBegin, GroupLayout.PREFERRED_SIZE, 119, GroupLayout.PREFERRED_SIZE)
-						.addComponent(txtBoxEmail, GroupLayout.PREFERRED_SIZE, 119, GroupLayout.PREFERRED_SIZE)
-						.addComponent(txtBoxpassword, GroupLayout.PREFERRED_SIZE, 119, GroupLayout.PREFERRED_SIZE)
-						.addComponent(txtBoxLastname, GroupLayout.PREFERRED_SIZE, 119, GroupLayout.PREFERRED_SIZE)
-						.addComponent(txtBoxFirstname, GroupLayout.PREFERRED_SIZE, 119, GroupLayout.PREFERRED_SIZE))
-					.addGap(157))
-		);
-		gl_contentPanel.setVerticalGroup(
-			gl_contentPanel.createParallelGroup(Alignment.LEADING)
-				.addGroup(gl_contentPanel.createSequentialGroup()
-					.addGap(78)
-					.addGroup(gl_contentPanel.createParallelGroup(Alignment.BASELINE)
-						.addComponent(txtBoxFirstname, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
-						.addComponent(lblFirstName))
-					.addPreferredGap(ComponentPlacement.RELATED)
-					.addGroup(gl_contentPanel.createParallelGroup(Alignment.BASELINE)
-						.addComponent(txtBoxLastname, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
-						.addComponent(lblNom))
-					.addPreferredGap(ComponentPlacement.RELATED)
-					.addGroup(gl_contentPanel.createParallelGroup(Alignment.BASELINE)
-						.addComponent(txtBoxpassword, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
-						.addComponent(lblMotDePasse))
-					.addPreferredGap(ComponentPlacement.RELATED)
-					.addGroup(gl_contentPanel.createParallelGroup(Alignment.BASELINE)
-						.addComponent(txtBoxEmail, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
-						.addComponent(lblEmail))
-					.addPreferredGap(ComponentPlacement.RELATED)
-					.addGroup(gl_contentPanel.createParallelGroup(Alignment.BASELINE)
-						.addComponent(txtBoxDateBegin, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
-						.addComponent(lblDateBegin))
-					.addPreferredGap(ComponentPlacement.RELATED)
-					.addGroup(gl_contentPanel.createParallelGroup(Alignment.BASELINE)
-						.addComponent(txtBoxDateEnd, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
-						.addComponent(lblDateEnd))
-					.addContainerGap(104, Short.MAX_VALUE))
-		);
-		contentPanel.setLayout(gl_contentPanel);
+		JPanel dateBeginPanel = new JPanel();
+		calendarGridLayout.add(dateBeginPanel);
+		dateBeginPanel.setLayout(new BorderLayout(0, 0));
+		
+		JLabel lblDateBegin = new JLabel("Date de d\u00E9but :");
+		dateBeginPanel.add(lblDateBegin, BorderLayout.NORTH);
+		calendar calBegin = new calendar();
+		dateBeginPanel.add(calBegin, BorderLayout.CENTER);
+		
+		JPanel dateEndPanel = new JPanel();
+		calendarGridLayout.add(dateEndPanel);
+		dateEndPanel.setLayout(new BorderLayout(0, 0));
+		
+		JLabel lblDateEnd = new JLabel("Date de d\u00E9but :");
+		dateEndPanel.add(lblDateEnd, BorderLayout.NORTH);
+		calendar calEnd = new calendar();
+		dateEndPanel.add(calEnd);
 		{
-			JPanel buttonPane = new JPanel();
-			buttonPane.setLayout(new FlowLayout(FlowLayout.RIGHT));
-			getContentPane().add(buttonPane, BorderLayout.SOUTH);
+			JPanel endPanel = new JPanel();
+			rightPanel.add(endPanel, BorderLayout.SOUTH);
+			endPanel.setLayout(new GridLayout(2, 1, 0, 0));
+			
+			JPanel detailsPanel = new JPanel();
+			endPanel.add(detailsPanel);
+			
+			JLabel lblNewLabel_1 = new JLabel("Carte associ\u00E9e:");
+			detailsPanel.add(lblNewLabel_1);
+			
+			JComboBox comboBox = new JComboBox();
+			detailsPanel.add(comboBox);
+			
+			JCheckBox chckbxNewCheckBox = new JCheckBox("D\u00E9sactivation de l'utilisateur");
+			detailsPanel.add(chckbxNewCheckBox);
+			
+			JPanel buttonPanel = new JPanel();
+			endPanel.add(buttonPanel);
 			{
-				JButton okButton = new JButton("OK");
+				cancelButton = new JButton("Cancel");
+				buttonPanel.add(cancelButton);
+				cancelButton.setAlignmentX(Component.CENTER_ALIGNMENT);
+				cancelButton.setActionCommand("Cancel");
+			}
+			{
+				okButton = new JButton("OK");
+				buttonPanel.add(okButton);
+				okButton.setAlignmentX(Component.CENTER_ALIGNMENT);
 				okButton.setActionCommand("OK");
-				buttonPane.add(okButton);
 				getRootPane().setDefaultButton(okButton);
 			}
-			{
-				JButton cancelButton = new JButton("Cancel");
-				cancelButton.setActionCommand("Cancel");
-				buttonPane.add(cancelButton);
-			}
 		}
+		
+
+		/*
+		JLabel lblDateBegin = new JLabel("Date de d\u00E9but :");
+		rightContentPanel.add(lblDateBegin);
+		txtBoxDateBegin = new JTextField(dateBegin);
+		txtBoxDateBegin.setHorizontalAlignment(SwingConstants.CENTER);
+		rightContentPanel.add(txtBoxDateBegin);
+		txtBoxDateBegin.setColumns(10);
+		
+		JLabel lblDateEnd = new JLabel("Date de fin :");
+		lblDateEnd.setHorizontalAlignment(SwingConstants.CENTER);
+		rightContentPanel.add(lblDateEnd);
+		txtBoxDateEnd = new JTextField(dateEnd);
+		txtBoxDateEnd.setHorizontalAlignment(SwingConstants.CENTER);
+		rightContentPanel.add(txtBoxDateEnd);
+		txtBoxDateEnd.setColumns(10);*/
 	}
 }
