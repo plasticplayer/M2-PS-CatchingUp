d2b2129682c024d20d5bd1da2b07453c2b5562bc
root <root@raspCours.(none)>
2015-06-26 14:07:05 +0000

    Demo RPI

--- Software/ModuleEnregistrement/Module/header/TraitementImage.h
+++ Software/ModuleEnregistrement/Module/header/TraitementImage.h
@@ -44,6 +44,25 @@ public :
 		memcpy(this->data,data,(cols*rows*channels));
 
 	}
+	Mat(unsigned int _cols,unsigned int _rows, unsigned char _channels,unsigned char * dataIn,int ratio)
+	{
+		this->cols = _cols/ratio;
+		this->rows = _rows/ratio;
+		this->channels = _channels;
+		this->data = new unsigned char[(cols*rows*channels)];
+		unsigned int x=0,y=0;
+		for ( unsigned int i = 0 ; i < ( _rows ); i+=ratio){
+			for ( unsigned int j = 0; j < ( _cols ) ; j+=ratio ){
+				*(this->data + (((y*cols)+x)*channels)+0) = *(dataIn +(((i*_cols)+j)*channels)+0);
+				*(this->data + (((y*cols)+x)*channels)+1) = *(dataIn +(((i*_cols)+j)*channels)+1);
+				*(this->data + (((y*cols)+x)*channels)+2) = *(dataIn +(((i*_cols)+j)*channels)+2);
+				x++;
+			}
+			y++;
+			x = 0;
+		}
+
+	}
 	~Mat()
 	{
 		if(data != NULL)
@@ -59,7 +78,27 @@ public :
 		this->data = new unsigned char[cols*rows*channels];
 		memcpy(this->data,src.data,(cols * rows * channels));
 	}
-/*	static Mat clone(Mat& src)
+	void clone(Mat& src, int ratio)
+	{
+		if(data != NULL)
+			delete[] data;
+		this->cols = src.cols/ratio;
+		this->rows = src.rows/ratio;
+		this->channels = src.channels;
+		this->data = new unsigned char[(cols*rows*channels)];
+		unsigned int x=0,y=0;
+		for ( unsigned int i = 0 ; i < ( src.rows ); i+=ratio){
+			for ( unsigned int j = 0; j < ( src.cols ) ; j+=ratio ){
+				*(this->data + (((y*cols)+x)*channels)+0) = *(src.data +(((i*src.cols)+j)*channels)+0);
+				*(this->data + (((y*cols)+x)*channels)+1) = *(src.data +(((i*src.cols)+j)*channels)+1);
+				*(this->data + (((y*cols)+x)*channels)+2) = *(src.data +(((i*src.cols)+j)*channels)+2);
+				x++;
+			}
+			y++;
+			x = 0;
+		}
+	}
+	/*static Mat clone(Mat& src)
 	{
 		Mat m();
 		m.clone(src);
